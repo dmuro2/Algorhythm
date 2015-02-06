d1ea37d2d788e460370d8c371c3166f888fcbf23
David Muro II <dmuro2@gmail.com>
2015-02-06 11:08:16 -0800

    Final refinements

--- Algorhythm/Algorhythm/PlaylistMasterViewController.swift
+++ Algorhythm/Algorhythm/PlaylistMasterViewController.swift
@@ -9,15 +9,36 @@
 import UIKit
 
 class PlaylistMasterViewController: UIViewController {
-
+    
+    
+    var playlistArray: [UIImageView] = []
+    
     @IBOutlet weak var playlistImageView0: UIImageView!
+    @IBOutlet weak var playlistImageView1: UIImageView!
+    @IBOutlet weak var playlistImageView2: UIImageView!
+    @IBOutlet weak var playlistImageView3: UIImageView!
+    @IBOutlet weak var playlistImageView4: UIImageView!
+    @IBOutlet weak var playlistImageView5: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let playlist = Playlist(index: 0)
-        playlistImageView0.image = playlist.icon
-        playlistImageView0.backgroundColor = playlist.backgroundColor
+        
+        playlistArray += [playlistImageView0, playlistImageView1, playlistImageView2, playlistImageView3, playlistImageView4, playlistImageView5]
+        
+        
+        for index in 0..<playlistArray.count {
+            
+            //set playlist instance
+            let playlist = Playlist(index: index)
+            let playlistImageView = playlistArray[index]
+            
+            playlistImageView.image = playlist.icon
+            playlistImageView.backgroundColor = playlist.backgroundColor
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,8 +49,14 @@ class PlaylistMasterViewController: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showPlaylistDetailSegue" {
-            let playlistDetailController = segue.destinationViewController as PlaylistDetailViewController
-            playlistDetailController.playlist = Playlist(index: 0)
+            
+            let playlistImageView = sender!.view as UIImageView
+            if let index = find(playlistArray, playlistImageView) {
+                
+                let playlistDetailController = segue.destinationViewController as PlaylistDetailViewController
+                playlistDetailController.playlist = Playlist(index: index)
+                
+            }
         }
     }
     
@@ -40,10 +67,7 @@ class PlaylistMasterViewController: UIViewController {
         
     }
     
-    
-    
-    
-    
+
     
 }
 
